51d036313ae15ce4e81fec8dddfc3eefea944287
fischeversenker <felix.fischeversenker@gmail.com>
2014-06-30 21:01:17 +0200

    entry point

---
+++ tests/esprima/trunk/esprimaParserAdapter.js
@@ -0,0 +1,8 @@
+define(function (require, exports, module) {
+    var fuck = function(){
+        console.clear();
+        console.log('nice')
+    };
+    
+    exports.fuck = fuck;
+});
---
+++ tests/esprima/trunk/main.js
@@ -0,0 +1,9 @@
+define(function (require, exports, module) {
+    
+    "use strict";
+    console.clear()
+    //var parserAdapter = require('esprimaParserAdapter');
+    //parserAdapter.fuck();
+
+
+});
